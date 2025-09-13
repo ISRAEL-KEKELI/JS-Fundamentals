6ded7273f953994285f5dfaa67cd3259c86b871c
KEKELI <israelkekelialorvor@gmail.com>
2025-09-13 20:26:59 +0000

    Create  6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array containing the strings to print
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a loop to print all the elements in the array
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
