091cbae74bac19dbc2b65019da51f454bef5e416
KEKELI <israelkekelialorvor@gmail.com>
2025-09-13 20:41:38 +0000

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,13 @@
-// Get the first argument passed to the script
+#!/usr/bin/node
+
 const x = parseInt(process.argv[2]);
 
-// Check if the argument is a valid number and is greater than 0
-if (isNaN(x) || x <= 0) {
-    console.log("Missing number of occurrences");
-} else {
-    // Loop to print "C is fun" x times
-    for (let i = 0; i < x; i++) {
-        console.log("C is fun");
-    }
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else if (x > 0) {
+  let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
+  }
 }
